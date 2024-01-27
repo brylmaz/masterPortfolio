5feb0e73fc921d683f9ca1a62974217ece7fd966
Barış <33360733+brylmaz@users.noreply.github.com>
2024-01-27 15:52:38 +0300

    -

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const seo = {
 //Home Page
 const greeting = {
   title: "Barış YILMAZ",
-  logo_name: "Barış YILMAZ",
+  logo_name: "AshutoshHathidara",
   nickname: "brylmaz",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
